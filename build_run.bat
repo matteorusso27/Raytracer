d677e928789613639ccdd111a78a41672a38a921
matteorusso27 <matteo2795@gmail.com>
2026-01-10 22:15:19 +0100

    Initial commit for c++ ray tracer

---
+++ build_run.bat
@@ -0,0 +1,2 @@
+cmake --build build --config release
+build\Release\Raytracing.exe > images/image.ppm
---
+++ interval.h
@@ -0,0 +1,45 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include <limits>  // for std::numeric_limits
+
+class interval {
+public:
+    double min, max;
+
+    // Default constructor: empty interval
+    interval() 
+        : min(std::numeric_limits<double>::infinity()), 
+          max(-std::numeric_limits<double>::infinity()) {}
+
+    // Constructor with bounds
+    interval(double min_val, double max_val) 
+        : min(min_val), max(max_val) {}
+
+    // Returns the size of the interval
+    double size() const {
+        return max - min;
+    }
+
+    // Check if a value is inside [min, max]
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    // Check if a value is strictly inside (min, max)
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    // Static constants for convenience
+    static const interval empty;
+    static const interval universe;
+};
+
+// Definition of static constants
+const interval interval::empty = interval(
+    std::numeric_limits<double>::infinity(),
+    -std::numeric_limits<double>::infinity()
+);
+
+#endif
